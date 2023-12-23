0032dba457742116e14c015ee43208e15a5925e5
Abdelghani <143917624+babdellghani@users.noreply.github.com>
2023-12-23 23:38:04 +0100

    Update layout.php

--- view/pages/layout.php
+++ view/pages/layout.php
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
 
+    <!-- Render All Elements Normally -->
+    <link rel="stylesheet" href="view/layout/css/normalize.css">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="view/layout/css/all.min.css">
+    <!-- Main Template CSS -->
+    <link rel="stylesheet" href="view/layout/css/style.css">
+</head>
+<body>
+
+<?= $content ?>
+
+<script src="view/layout/js/script.js"></script>
+</body>
+</html>
