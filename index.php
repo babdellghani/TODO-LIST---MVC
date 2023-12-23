73f1b37a52f35191c01b9cf0080f2409889ce411
Abdelghani <143917624+babdellghani@users.noreply.github.com>
2023-12-23 23:38:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
+<?php
+require_once './Controller/todoController.php';
 
+if (isset($_GET['action'])) {
+    $action = filter_var($_GET['action'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    switch ($action) {
+        case 'create':
+            createAction();
+            break;
+        case 'edit':
+            editAction();
+            break;
+        case 'update':
+            updateAction();
+            break;
+        case 'destroy':
+            destroyAction();
+            break;
+        case 'check':
+            checkAction();
+            break;
+        default:
+            indexAction();
+            break;
+        }
+}  else {
+    indexAction();
+}
